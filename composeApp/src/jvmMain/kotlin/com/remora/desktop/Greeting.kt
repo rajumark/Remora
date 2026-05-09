0d256fbbd68dbc11e2dab3594baaf48e83dd0dc1
rajumark <raju348636@gmail.com>
2026-05-09 10:41:35 +0530

    first commit

---
+++ composeApp/src/jvmMain/kotlin/com/remora/desktop/Greeting.kt
@@ -0,0 +1,9 @@
+package com.remora.desktop
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/jvmMain/kotlin/com/remora/desktop/Platform.kt
@@ -0,0 +1,7 @@
+package com.remora.desktop
+
+class JVMPlatform {
+    val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+fun getPlatform() = JVMPlatform()
